7155b80da09087359aa9b64c8cd33380da9a6731
tunanepal <tunanepal776@gmail.com>
2026-09-22 15:12:18 +0545

    Update auth.js

--- auth.js
+++ auth.js
@@ -354,7 +354,7 @@ async function land(out) {
   captcha = null; capChoice = null;
   setToken(out.token);
   applyTheme(out.player.theme || savedTheme());
-   import('./notifications.js').then(n => n.initNotifications());
+   import('./notifications.js').then(n => n.initNotifications(out.player.id));
   toast(out.created
     ? `Welcome to Tunanepal, ${out.player.name.split(' ')[0]}.`
     : `Welcome back, ${out.player.name.split(' ')[0]}.`, 'good');
